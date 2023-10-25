602052e3700a0eca6fb2d1166dd0a6448da9e6c2
James Waddington <sugarfrosted2005@gmail.com>
2023-10-25 03:24:13 -0500

    rename variable

--- LucyAndLily/Program.cs
+++ LucyAndLily/Program.cs
@@ -12,12 +12,12 @@ namespace LucyAndLily
     {
         static void Main(string[] args)
         {
-            var ass = Expr.Parse("cos(2 / 5 * pi) + cos(4 / 5 * pi) - cos(6 / 5 * pi) - cos(8 / 5 * pi)");
-            Console.WriteLine(ass.TrigonometricContract());
-            Console.WriteLine(ass.TrigonometricExpand());
-            Console.WriteLine(ass.TrigonometricSimplify());
-            Console.WriteLine(ass.Evaluate(new Dictionary<string, FloatingPoint>()));
-            Console.WriteLine(ass.RationalReduce());
+            var expression = Expr.Parse("cos(2 / 5 * pi) + cos(4 / 5 * pi) - cos(6 / 5 * pi) - cos(8 / 5 * pi)");
+            Console.WriteLine(expression.TrigonometricContract());
+            Console.WriteLine(expression.TrigonometricExpand());
+            Console.WriteLine(expression.TrigonometricSimplify());
+            Console.WriteLine(expression.Evaluate(new Dictionary<string, FloatingPoint>()));
+            Console.WriteLine(expression.RationalReduce());
         }
     }
 }
